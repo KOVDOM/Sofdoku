5cacacfa596cb1bd6160a0b4b9579f52495a6291
KOVDOM <94444372+KOVDOM@users.noreply.github.com>
2022-05-27 07:49:59 +0200

    Add files via upload

---
+++ WindowsFormsApp5/ConsoleApp1/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class Feladvany
+    {
+        public string Kezdo { get; private set; }
+        public int Meret { get; private set; }
+
+        public Feladvany(string sor)
+        {
+            Kezdo = sor;
+            Meret = Convert.ToInt32(Math.Sqrt(sor.Length));
+        }
+
+        public void Kirajzol()
+        {
+            for (int i = 0; i < Kezdo.Length; i++)
+            {
+                if (Kezdo[i] == '0')
+                {
+                    Console.Write(".");
+                }
+                else
+                {
+                    Console.Write(Kezdo[i]);
+                }
+                if (i % Meret == Meret - 1)
+                {
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+
+    
+    class Program
+    {
+        static List<Feladvany> feladvanyok = new List<Feladvany>();
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader("feladvanyok.txt");
+            while (!sr.EndOfStream)
+            {
+                Feladvany f = new Feladvany(sr.ReadLine());
+                feladvanyok.Add(f);
+            }
+
+            Console.WriteLine("Melyik méretett keresed? ");
+            int db = 0;
+            int keresettszam = Convert.ToInt32(Console.ReadLine());
+            List<Feladvany> egymeretu = new List<Feladvany>();
+            foreach (var item in feladvanyok)
+            {
+                if (item.Meret==keresettszam)
+                {
+                    db++;
+                    egymeretu.Add(item);
+                }
+            }
+            Console.WriteLine(db);
+            Console.WriteLine();
+            Random r = new Random();
+            int index = r.Next(0, egymeretu.Count);
+            egymeretu[index].Kirajzol();
+            Console.WriteLine();
+
+            int osszes = egymeretu[index].Meret* egymeretu[index].Meret;
+            int kitoltve = 0;
+            foreach (var item in egymeretu[index].Kezdo)
+            {
+                if (item!='0')
+                {
+                    kitoltve++;
+                }
+            }
+            double szazalek = ((double)kitoltve / osszes) * 100;
+            Console.WriteLine((int)szazalek+"%");
+            string fajlnev = "sodoku";
+            fajlnev += keresettszam.ToString();
+            fajlnev += ".txt";
+            StreamWriter sw = new StreamWriter(fajlnev);
+            foreach (var item in egymeretu)
+            {
+                sw.WriteLine(item.Kezdo);
+            }
+            sw.Close();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ WindowsFormsApp5/WindowsFormsApp5/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox2.Text.Length==i*i)
+            {
+                MessageBox.Show("A feladvány megfelelő hosszúságú!");
+            }
+            else if (textBox2.Text.Length>i*i)
+            {
+                MessageBox.Show("A feladvány hosszú: törlendő " + (textBox2.Text.Length > i * i)+" számjegy");
+            }
+            else if (true)
+            {
+                MessageBox.Show("A feladvány túl rövid: törlendő " + (i * i - textBox2.Text.Length) + " számjegy");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (i < 9)
+            {
+                i++;
+                textBox1.Text = i.ToString();
+            }
+        }
+
+        int i = 4;  
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (i>4)
+            {
+                i--;
+                textBox1.Text = i.ToString();
+            }
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            label3.Text = "Hossz: " + textBox2.Text.Length;
+        }
+    }
+}
